1de4964d5af31db450acefd56aad6d706bc0da97
Abhishek Yadav <monuy8830@gmail.com>
2025-06-14 15:31:26 +0530

    Refactor ShirtGallery component for improved readability and structure

--- src/components/shirt-gallery/ShirtGallery.js
+++ src/components/shirt-gallery/ShirtGallery.js
@@ -1,9 +1,16 @@
+"use client"
 import React from 'react';
 import ShirtImages from './ShirtImages';
+import { useRouter } from 'next/navigation';
 
 const ShirtGallery = () => {
+  const router = useRouter()
   return (
-    <div data-aos="fade-right" data-aos-duration="1000" className="flex flex-col md:flex-row justify-between items-center max-w-3xl md:max-w-5xl lg:max-w-7xl mx-auto px-4 sm:px-6 md:px-12 py-12 sm:py-16">
+    <div
+      data-aos="fade-right"
+      data-aos-duration="1000"
+      className="flex flex-col md:flex-row justify-between items-center max-w-3xl md:max-w-5xl lg:max-w-7xl mx-auto px-4 sm:px-6 md:px-12 py-12 sm:py-16"
+    >
       {/* Left Section */}
       <div className="w-full md:w-1/2 text-center md:text-left mb-12 md:mb-0">
         <p className="text-gray-500 text-lg sm:text-xl mb-2">
@@ -13,9 +20,17 @@ const ShirtGallery = () => {
           Comfortable Shirts
         </h2>
         <p className="text-gray-700 text-lg sm:text-[22px] mb-6">
-          We are launching our new collection of cotton shirts at affordable rates.
+          We are launching our new collection of cotton shirts at affordable
+          rates.
         </p>
-        <button className="bg-black text-white px-6 sm:px-8 py-3 rounded-md text-sm font-semibold hover:bg-gray-800 transition">
+        <button
+          className="bg-black text-white px-6 sm:px-8 py-3 rounded-md text-sm font-semibold hover:bg-gray-800 transition"
+          onClick={() =>
+            router.push(
+              "/search?category=a-men-s-cotton-shirt-&_id=67fe0b82f18106004633a7e0"
+            )
+          }
+        >
           SHOP NOW
         </button>
       </div>
